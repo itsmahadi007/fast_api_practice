d527ffdad5677e2da3b6b0b143b6a8c2e37afc3e
Mahadi Hassan <itsmahadi@gmail.com>
2023-02-18 21:48:47 +0600

    update

---
+++ main.py
@@ -0,0 +1,138 @@
+from enum import Enum
+
+from fastapi import FastAPI, Request, Query, Path, Body
+from pydantic import BaseModel, Required
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
+
+
+@app.get("/ip")
+async def get_ip(request: Request):
+    return {"ip": request.client.host}
+
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
+
+
+@app.get("/files/{file_path:path}")
+async def read_file(file_path: str):
+    return {"file_path": file_path}
+
+
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+
+
+@app.post("/items/")
+async def create_item(item: Item):
+    item_dict = item.dict()
+    if item.tax:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return {"item_dict": item_dict, **item.dict()}
+
+
+@app.put("/items/{item_id}")
+async def create_item(item_id: int, item: Item, q: str | None = None):
+    result = {"item_id": item_id, **item.dict()}
+    if q:
+        result.update({"q": q})
+    return result
+
+
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10, q: str | None = Query(default=None, max_length=5)):
+    results = fake_items_db[0]
+    if q:
+        results.update({"q": q})
+    return results
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: str, q: str | None = None):
+    if q:
+        return {"item_id": item_id, "q": q}
+    item = {"item_id": item_id, "q": q}
+    return item
+
+
+@app.get("/items_2/")
+async def read_items(
+        q: str = Query(default=Required, min_length=3, max_length=50)
+):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
+
+
+@app.get("/items3/")
+async def read_items(
+        *,
+        q: str | None = Query(
+            default=None,
+            title="Query string",
+            description="Query string for the items to search in the database that have a good match",
+            min_length=3,
+        )
+):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
+
+
+@app.put("/items4/{item_id}")
+async def update_item(
+        *,
+        item_id: int = Path(title="The ID of the item to get", ge=0, le=1000),
+        q: str | None = None,
+        item: Item | None = None,
+):
+    results = {"item_id": item_id}
+    if q:
+        results.update({"q": q})
+    if item:
+        results.update({"item": item})
+    return results
+
+
+class User(BaseModel):
+    username: str
+    full_name: str | None = None
+
+
+@app.put("/items5/{item_id}")
+async def update_item(item_id: int, item: Item, user: User, importance: int = Body()):
+    results = {"item_id": item_id, "item": item, "user": user, "importance": importance}
+    return results

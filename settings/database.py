e9a4c0db5e15e2c22a30bc356d962fb782f3122b
Mahadi Hassan <itsmahadi@gmail.com>
2025-02-02 02:23:22 +0600

    update

--- settings/database.py
+++ settings/database.py
@@ -3,14 +3,9 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./fastapi_practice.db"
-
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 DBBase = declarative_base()
-
 
 def get_db():
     db = SessionLocal()

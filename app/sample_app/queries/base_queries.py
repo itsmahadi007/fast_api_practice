ec7cb9bf40df1f71850a997f8e65f009d075b1b1
Mahadi Hassan <itsmahadi@gmail.com>
2025-02-02 02:27:51 +0600

    Update base_queries.py

--- app/sample_app/queries/base_queries.py
+++ app/sample_app/queries/base_queries.py
@@ -11,14 +11,19 @@ def get_books_by_title(db: Session, title: str):
     try:
         return db.query(BooksModel).filter(BooksModel.title == title).first()
     except Exception as e:
-        logger.warning(f"Error: {e} -------------")
+        logger.warning(f"Error: {e}")
         return None
 
 
-def create_books(db: Session, book: BookBase) -> BooksModel:
+def create_books(db: Session, book: BookBase) -> BooksModel | str:
     db_book = BooksModel(**book.model_dump())
-    db.add(db_book)
-    db.commit()
+    try:
+        db.add(db_book)
+        db.commit()
+    except Exception as e:
+        db.rollback()
+        logger.error(f"Error during book creation: {e}")
+        return str(e)
     db.refresh(db_book)
     return db_book
 
